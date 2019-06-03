fb2664ddbb989a774e0fc8e8abb35ffa89864b4c
root <root@ip-172-31-21-47.us-east-2.compute.internal>
2019-06-03 18:24:04 +0000

    i did it

--- app.js
+++ app.js
@@ -5,25 +5,16 @@ var fs = require('fs');
 var path = require('path');
 var ejs = require('ejs');
 var request = require('request');
-
+var baseRouter = require('./routes/route');
 
 app.locals.pretty = true
 app.use(express.static('public')); 
 app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+app.use('/',baseRouter);
 
 app.set('view engine', 'ejs');//ejs 템플릿 엔진  연동 
-app.get('/', function (req, res, ) { 
- 
-   var data = req.body; 
-   res.render('index', {name: data});
-  
-});
-
-
-
 
 app.listen(80, function(){ 
      console.log('App Listening on port 80'); 
--- routes/route.js
+++ routes/route.js
@@ -8,10 +8,10 @@ var router = express.Router();
 router.get('/',function(req,res,next){
     var data = req.body; 
     res.render('index', {name: data});
-next();
-});
 
-
-request.post('http://175.195.56.132:3000/sensordata',function(err,res,body){
-    jsonfile.writefile('sensordata', res.body);
+request.get('http://175.195.56.132:3000/sensordata',function(err,res,body){
+    jsonfile.writeFile('sensordata', res.body);
 });
+});
+module.exports = router;
+
