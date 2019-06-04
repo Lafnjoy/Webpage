84dcbf5c8667d7267deafd7478512cc3d69e591e
Lafnjoy <sunstar1023@naver.com>
2019-06-04 17:03:56 +0900

    Semi finial

--- routes/route.js
+++ routes/route.js
@@ -2,16 +2,22 @@ var express = require('express');
 var request = require('request')
 var bodyParser = require('body-parser');
 var jsonfile = require('jsonfile');
+var sensorD;
 
+request.get('http://175.195.56.132:3000/sensordata',function(err,res,body){
+    sensorD = res.body;
+   
+});
 
 var router = express.Router();
 router.get('/',function(req,res,next){
     var data = req.body; 
-    res.render('index', {name: data});
-
+    
 request.get('http://175.195.56.132:3000/sensordata',function(err,res,body){
-    jsonfile.writeFile('sensordata.json', res.body);
+    sensorD = res.body;
 });
+
+res.render('index', {data: sensorD});
 });
 module.exports = router;
 
